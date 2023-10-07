28c95eaced2035f2aab2bdd35a912cd8e8045207
qasemB <mahdicmptr@gmail.com>
2023-10-07 14:22:39 +0330

    after ok warnings

--- app.js
+++ app.js
@@ -7,12 +7,8 @@ document
     document.getElementById("upload_submit").disabled = false
   });
 
-// test
-
 const allFilesTranslator = async () => {
-  document
-    .getElementById("all_file")
-    .setAttribute("max", document.getElementById("phpFile").files.length);
+  document.getElementById("all_file").max = document.getElementById("phpFile").files.length
   for (const file of document.getElementById("phpFile").files) {
     const res = await oneOfAllFiles(file);
     console.log(res);
@@ -35,14 +31,12 @@ const allFilesTranslator = async () => {
     linkElement.click();
 
     const oldVal = document.getElementById("all_file").getAttribute("value");
-    document
-      .getElementById("all_file")
-      .setAttribute("value", parseInt(oldVal) + 1);
+    document.getElementById("all_file").value = parseInt(oldVal) + 1
   }
 };
 
 const oneOfAllFiles = async (file) => {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     const reader = new FileReader();
     reader.onload = async function (event) {
       let phpCode = event.target.result;
@@ -55,7 +49,7 @@ const oneOfAllFiles = async (file) => {
       phpCode = phpCode.split("]")[0];
       phpCode = phpCode.split(",");
 
-      phpCode = phpCode.filter((str) => str.trim() != "");
+      phpCode = phpCode.filter((str) => str.trim() !== "");
       phpCode = phpCode.map((str) => {
         str = str.replace(/[\/\n,"']/g, "").trim();
         str = str.split("=>")[0];
@@ -71,7 +65,7 @@ const oneOfAllFiles = async (file) => {
 async function oneFileTranslator(strArr) {
   document.getElementById("one_file").setAttribute("max", strArr.length);
   const newArr = [];
-  for (item of strArr) {
+  for (let item of strArr) {
     // https://one-api.ir/translate/?token=576329:651ea8266c711&action=google&lang=fa&q=admin%20tags_delete
     const str = item.replaceAll("_", " ")
     let res = await fetch(
@@ -80,9 +74,7 @@ async function oneFileTranslator(strArr) {
     res = await res.json();
     newArr.push(`'${item.trim()}' => '${res.result}'`);
     const oldVal = document.getElementById("one_file").getAttribute("value");
-    document
-      .getElementById("one_file")
-      .setAttribute("value", parseInt(oldVal) + 1);
+    document.getElementById("one_file").value = parseInt(oldVal) + 1
   }
-  return newArr; // I want the next request, to be send after received this reponse
+  return newArr;
 }
